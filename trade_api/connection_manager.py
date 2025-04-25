df2802d0238a1bb3d43d821b51de77a4ec0f1ff2
austinsnyder <rsnyder.austin@gmail.com>
2025-04-25 09:17:38 -0500

    c

--- trade_api/connection_manager.py
+++ trade_api/connection_manager.py
@@ -1,6 +1,18 @@
 
+import requests
 
-class ConnectionManager:
 
-    
+class TradeConnectionManager:
+
+    base_url = "https://www.pathofexile.com/api/trade2/"
+    search_url = base_url + "search/poe2/Standard"
+    fetch_url = base_url + "fetch/"
+
+    header = {
+        'Content-Type': 'application/json',
+        'User-Agent': '5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
+        'Cookie': f'POESESSID={your_poesessid}',
+        'Accept': '*/*',
+        'Connection': 'keep-alive'
+    }
 
