7f408d1b55d65e95e66f7b874073917324cec7b6
rsnyderaustin <114520816+rsnyderaustin@users.noreply.github.com>
2025-04-29 16:10:28 -0500

    Update crafting_engine.py

--- crafting/crafting_engine.py
+++ crafting/crafting_engine.py
@@ -1,12 +1,14 @@
 
 from .base_type_mods_manager import BaseTypeModsManager
+from .outcomes import CraftingOutcome
 from things import Currency
 from things.items import Modifiable
 
 class CraftingEngine:
 
-    def __init__(self, base_type_mods_manager):
+    def __init__(self, base_type_mods_manager: BaseTypeModsManager):
+        self.mods_manager = base_type_mods_manager
 
     @classmethod
-    def roll_new_modifier(cls, item: Modifiable, force_type: str = None):
+    def roll_new_modifier(cls, item: Modifiable, force_type: str = None) -> list[CraftingOutcome]:
 
