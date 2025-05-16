e2e9036f8b42bac72e4abf21e47db89f7e3a8685
austinsnyder <rsnyder.austin@gmail.com>
2025-05-16 17:47:50 -0500

    c

--- crafting_model/environment.py
+++ crafting_model/environment.py
@@ -1,7 +1,7 @@
 import copy
 
-import gym
-from gym import spaces
+import gymnasium as gym
+from gymnasium import spaces
 import numpy as np
 import random
 
--- crafting_model/trainer.py
+++ crafting_model/trainer.py
@@ -1,15 +1,20 @@
 
-from stable-baselines3 import PPO
-from stable-baselines3.common.env_checker import check_env
+from stable_baselines3 import PPO
+from stable_baselines3.common.env_checker import check_env
 
+import file_management
+from file_management import FileKey
 from . import environment
+
 
 class RlTrainer:
 
-
-    def __init__(self):
-        self.env = environment.CraftingEnvironment()
-        check_env(self.env)
+    @classmethod
+    def train(cls, listing):
+        price_predictor = file_management.FilesManager().model_data[FileKey.PRICE_PREDICT_MODEL]
+        env = environment.CraftingEnvironment(listing=listing,
+                                              price_predictor=price_predictor)
+        check_env(env)
 
         model = PPO("MultiInputPolicy", env, verbose=1)
         model.learn(total_timesteps=10000)
