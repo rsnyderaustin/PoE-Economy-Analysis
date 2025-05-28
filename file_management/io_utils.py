fa625f365f3d4ac3934b9fcef1f3f90a8cf4f872
rsnyderaustin <114520816+rsnyderaustin@users.noreply.github.com>
2025-05-28 18:17:29 -0500

    c

--- file_management/io_utils.py
+++ file_management/io_utils.py
@@ -4,15 +4,15 @@ from pathlib import Path
 
 
 class DataPath(Enum):
-    MODS = Path.cwd() / 'file_management/files/item_mods.pkl'
-    CURRENCY_CONVERSIONS = Path.cwd() / 'file_management/files/currency_prices.csv'
-    MARKET_SCAN = Path.cwd() / 'file_management/files/market_scan.json'
-    POECD_BASES = Path.cwd() / 'file_management/files/poecd_bases.json'
-    POECD_STATS = Path.cwd() / 'file_management/files/poecd_stats.json'
-    OFFICIAL_STATIC = Path.cwd() / 'file_management/files/official_static.json'
-    OFFICIAL_STATS = Path.cwd() / 'file_management/files/official_stats.json'
-    RAW_LISTINGS = Path.cwd() / 'file_management/files/raw_listings.json'
-    GLOBAL_POECD_MANAGER = Path.cwd() / 'file_management/files/global_poecd_manager.pkl'
+    MODS = Path.cwd() / 'file_management/dynamic_files/item_mods.pkl'
+    CURRENCY_CONVERSIONS = Path.cwd() / 'file_management/dynamic_files/currency_prices.csv'
+    MARKET_SCAN = Path.cwd() / 'file_management/dynamic_files/market_scan.json'
+    POECD_BASES = Path.cwd() / 'file_management/dynamic_files/poecd_bases.json'
+    POECD_STATS = Path.cwd() / 'file_management/dynamic_files/poecd_stats.json'
+    OFFICIAL_STATIC = Path.cwd() / 'file_management/dynamic_files/official_static.json'
+    OFFICIAL_STATS = Path.cwd() / 'file_management/dynamic_files/official_stats.json'
+    RAW_LISTINGS = Path.cwd() / 'file_management/dynamic_files/raw_listings.json'
+    GLOBAL_POECD_MANAGER = Path.cwd() / 'file_management/dynamic_files/global_poecd_manager.pkl'
 
 
 class ModelPath(Enum):
