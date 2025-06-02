90e929c23ac7e448bd15e40a57dd6f283a7c6760
rsnyderaustin <114520816+rsnyderaustin@users.noreply.github.com>
2025-06-01 21:29:35 -0500

    c

--- trade_api/handler.py
+++ trade_api/handler.py
@@ -24,7 +24,8 @@ class _ListingImportGatekeeper:
         dates_and_ids = psql_manager.fetch_columns_data(table_name='listings',
                                                         columns=['date_fetched', 'listing_id'])
         self.keys = {
-            (listing_id, date_fetched) for listing_id, date_fetched in zip(dates_and_ids['listing_id'], dates_and_ids['date_fetched'])
+            (listing_id, date_fetched)
+            for listing_id, date_fetched in zip(dates_and_ids['listing_id'], dates_and_ids['date_fetched'])
         }
 
     def listing_is_valid(self, listing_id: str, date_fetched: datetime, register_if_valid=True) -> bool:
